f1a93af4f379b734149b43af60d508fd1dac7f57
carlosOlcina <carlosolcina23@gmail.com>
2026-03-19 11:39:36 +0100

    feat: integrate new components into App and replace header/hero with standalone versions

--- apps/landing/src/app/app.ts
+++ apps/landing/src/app/app.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { Logo } from './components/atoms/logo/logo';
+import colorsBrand from './data/colors'
+import { Title1 } from './components/atoms/title-1/title-1';
+import { HeroSection } from './components/organism/hero-section/hero-section';
+import { Header } from './components/organism/header/header';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule, Logo, Title1, HeroSection, Header],
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected title = 'landing';
+  colorLogo = colorsBrand.secondary;
 }
