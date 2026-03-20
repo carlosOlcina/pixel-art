8b36dda7cbdc77518514ae77dcdce374f6cb97ed
carlosOlcina <carlosolcina23@gmail.com>
2026-03-20 13:32:05 +0100

    refactor: extract project data to a separate file and integrate into ProjectsSection

--- apps/landing/src/app/components/organism/projects-section/projects-section.ts
+++ apps/landing/src/app/components/organism/projects-section/projects-section.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Title2 } from '../../atoms/title-2/title-2';
 import { NgStyle } from '@angular/common';
+import Projects from '../../../data/projects';
 
 @Component({
   selector: 'landing-projects-section',
@@ -10,24 +11,5 @@ import { NgStyle } from '@angular/common';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectsSection {
-  projects = [
-    {
-      id: 'rRAOXGvJk-Rv0gXalFCOj',
-      title: 'Ovalo',
-      description: 'Proyecto de bicicleta con diseño futurista',
-      image: '/ovalo.png',
-    },
-    {
-      id: 'YIoxlHK6AO33OfwshF7fJ',
-      title: 'Optihome',
-      description: 'Proyecto de landing page para empresa de domótica moderna',
-      image: 'optihome.png',
-    },
-    {
-      id: 'Mku2Xr3uJ-LF2VFHn53Xm',
-      title: 'ORA',
-      description: 'Proyecto de un reloj futurista que capta la luz solar',
-      image: 'ora.png',
-    },
-  ];
+  projects = Projects
 }
---
+++ apps/landing/src/app/data/projects.ts
@@ -0,0 +1,21 @@
+
+export default [
+  {
+    id: 'rRAOXGvJk-Rv0gXalFCOj',
+    title: 'Ovalo',
+    description: 'Proyecto de bicicleta con diseño futurista',
+    image: '/ovalo.png',
+  },
+  {
+    id: 'YIoxlHK6AO33OfwshF7fJ',
+    title: 'Optihome',
+    description: 'Proyecto de landing page para empresa de domótica moderna',
+    image: 'optihome.png',
+  },
+  {
+    id: 'Mku2Xr3uJ-LF2VFHn53Xm',
+    title: 'ORA',
+    description: 'Proyecto de un reloj futurista que capta la luz solar',
+    image: 'ora.png',
+  },
+];
