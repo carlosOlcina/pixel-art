7674c6cb7f32d39470094274e5100986afdef1ae
carlosOlcina <carlosolcina23@gmail.com>
2026-03-20 11:10:01 +0100

    feat: add standalone Paragraph component with template, styles, and logic

---
+++ apps/landing/src/app/components/atoms/paragraph/paragraph.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import colorsBrand from '../../../data/colors';
+import { NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'landing-paragraph',
+  imports: [NgStyle],
+  templateUrl: './paragraph.html',
+  styleUrl: './paragraph.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Paragraph {
+  color = input<string>(colorsBrand.neutral);
+}
