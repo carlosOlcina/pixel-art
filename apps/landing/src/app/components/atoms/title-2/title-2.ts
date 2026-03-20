5dd318186a616386714b09176d47dbf472e7dfce
carlosOlcina <carlosolcina23@gmail.com>
2026-03-20 10:43:16 +0100

    feat: add standalone Title-2 component with template, styles, and logic

---
+++ apps/landing/src/app/components/atoms/title-2/title-2.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import colorsBrand from '../../../data/colors';
+import { NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'landing-title-2',
+  imports: [NgStyle],
+  templateUrl: './title-2.html',
+  styleUrl: './title-2.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Title2 {
+  color = input<string>(colorsBrand.primary);
+}
