e9e0b3f606af005ec589c5b8c7ad4bc2d9045750
carlosOlcina <carlosolcina23@gmail.com>
2026-03-24 18:16:30 +0100

    feat: add global validation pipe with whitelist option and remove API global prefix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,18 +3,19 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = '';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    `Application is running on: http://localhost:${port}/${globalPrefix}`,
   );
 }
 
