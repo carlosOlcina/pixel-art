7f048d70adc918c000e72c1b448c71efd088863c
carlosOlcina <carlosolcina23@gmail.com>
2026-03-24 18:06:25 +0100

    feat: add ResponseDateDto with transformation and exposure decorators in the API app

---
+++ apps/api/src/app/dto/response-date.dto.ts
@@ -0,0 +1,23 @@
+import { Expose, Transform } from 'class-transformer';
+
+export class ResponseDateDto {
+  @Expose()
+  id: string;
+
+  @Expose()
+  phone: string;
+
+  @Expose()
+  date: string;
+
+  @Expose()
+  confirmed: boolean;
+
+  @Expose()
+  @Transform(({ value }) => new Date(value).toISOString())
+  createdAt: string;
+
+  @Transform(({ value }) => new Date(value).toISOString())
+  @Expose()
+  updatedAt: Date;
+}
