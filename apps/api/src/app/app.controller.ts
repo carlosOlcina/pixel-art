9b702d991e93803a1f249c285ec74f7b626a5ace
carlosOlcina <carlosolcina23@gmail.com>
2026-03-24 19:04:56 +0100

    feat: implement CRUD endpoints for `/date` in AppController

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,12 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { ResponseDateDto } from './dto/response-date.dto';
+import { CreateDateDto } from './dto/create-date.dto';
+import { UpdateDateDto } from './dto/update-date.dto';
 
-@Controller()
+@Controller('/date')
 export class AppController {
+  // IN REAL CASE SHOULD IMPLEMENT AUTH SYSTEM
+
   constructor(private readonly appService: AppService) {}
 
+  @Get(':id')
+  getDateById(@Param('id', ParseUUIDPipe) id: string): ResponseDateDto {
+    return this.appService.getDateById(id);
+  }
+
   @Get()
-  getData() {
-    return this.appService.getData();
+  getDates(): ResponseDateDto[] {
+    return this.appService.getDates();
+  }
+
+  @Post()
+  createDate(@Body() dto: CreateDateDto): ResponseDateDto {
+    return this.appService.createDate(dto);
+  }
+
+  @Patch(':id')
+  updateDate(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateDateDto,
+  ): ResponseDateDto {
+    return this.appService.updateDate(id, dto);
+  }
+
+  @Delete(':id')
+  deleteDate(@Param('id', ParseUUIDPipe) id: string): string {
+    return this.appService.deleteDate(id);
   }
 }
