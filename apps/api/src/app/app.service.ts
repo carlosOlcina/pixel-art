4891fb533f1e6c492cd0cd4e2ec14eb0f66a5ceb
carlosOlcina <carlosolcina23@gmail.com>
2026-03-24 19:05:02 +0100

    feat: add in-memory CRUD operations for dates in AppService

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateDateDto } from './dto/create-date.dto';
+import { UpdateDateDto } from './dto/update-date.dto';
+import { ResponseDateDto } from './dto/response-date.dto';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  dates: ResponseDateDto[] = [];
+
+  getDateById(id: string): ResponseDateDto {
+    const date = this.dates.find((date) => date.id === id);
+    if (!date) throw new NotFoundException('Date not found');
+    return date;
+  }
+
+  getDates(): ResponseDateDto[] {
+    return this.dates;
+  }
+
+  createDate(dto: CreateDateDto): ResponseDateDto {
+    const date = this.dates.push({
+      id: dto.id,
+      phone: dto.phone,
+      date: dto.date,
+      confirmed: false,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    });
+
+    return this.dates[date - 1];
+  }
+
+  updateDate(id: string, dto: UpdateDateDto): ResponseDateDto {
+    const date = this.dates.find((date) => date.id === id);
+
+    if (!date) throw new NotFoundException('Date not found');
+
+    const updatedDate = {
+      ...date,
+      ...dto,
+      updatedAt: new Date().toISOString(),
+    };
+
+    this.dates = this.dates.map((date) =>
+      date.id === id ? updatedDate : date,
+    );
+
+    return updatedDate;
+  }
+
+  deleteDate(id: string): string {
+    const date = this.dates.findIndex((date) => date.id === id);
+
+    if (date < 0) throw new NotFoundException('Date not found');
+
+    this.dates.splice(date, 1);
+
+    return id;
   }
 }
