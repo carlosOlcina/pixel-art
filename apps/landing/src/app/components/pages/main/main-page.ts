baf2baba6648ad086e639cb38481cf2215ce9a06
carlosOlcina <carlosolcina23@gmail.com>
2026-03-29 13:16:53 +0200

    feat: add `FormSection` component to the main page layout

--- apps/landing/src/app/components/pages/main/main-page.ts
+++ apps/landing/src/app/components/pages/main/main-page.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AboutUsSection } from '../../organism/about-us-section/about-us-section';
 import { HeroSection } from '../../organism/hero-section/hero-section';
 import { ProjectsSection } from '../../organism/projects-section/projects-section';
+import { FormSection } from '../../organism/form-section/form-section';
 
 @Component({
   selector: 'landing-main-page',
-  imports: [AboutUsSection, HeroSection, ProjectsSection],
+  imports: [AboutUsSection, HeroSection, ProjectsSection, FormSection],
   templateUrl: './main-page.html',
   styleUrl: './main-page.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
