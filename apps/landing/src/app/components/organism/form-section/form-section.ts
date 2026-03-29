aac2c45bb3954756051aecddc28a2e5b505142d7
carlosOlcina <carlosolcina23@gmail.com>
2026-03-29 12:29:19 +0200

    feat: implement `FormSection` component with form validation and styling

---
+++ apps/landing/src/app/components/organism/form-section/form-section.ts
@@ -0,0 +1,60 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+} from '@angular/core';
+import { Section } from '../section/section';
+import { ScrollSectionService } from '../../../services/scroll-section';
+import { NgClass } from '@angular/common';
+import { Title2 } from '../../atoms/title-2/title-2';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatTimepickerModule } from '@angular/material/timepicker';
+
+@Component({
+  selector: 'landing-form-section',
+  imports: [
+    Section,
+    NgClass,
+    Title2,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatTimepickerModule,
+  ],
+  providers: [provideNativeDateAdapter()],
+  templateUrl: './form-section.html',
+  styleUrl: './form-section.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+})
+export class FormSection {
+  scrollSectionService = inject(ScrollSectionService);
+
+  isNext = computed(() => this.scrollSectionService.nextSection() === 'form');
+
+  form = new FormGroup({
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[0-9+]+$/),
+    ]),
+    date: new FormControl<Date | null>(null, [Validators.required]),
+    time: new FormControl<Date | null>(null, [Validators.required]),
+  });
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+}
