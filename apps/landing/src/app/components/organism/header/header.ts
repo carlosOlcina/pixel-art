09dbbd561eb04c2a9859f28e5eebad526e920f19
carlosOlcina <carlosolcina23@gmail.com>
2026-04-13 13:24:27 +0200

    refactor: eliminar navegación del encabezado y limpiar código CSS

--- apps/landing/src/app/components/organism/header/header.ts
+++ apps/landing/src/app/components/organism/header/header.ts
@@ -9,25 +9,4 @@ import { HeaderLink } from '../../atoms/header-link/header-link';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
-export class Header {
-  readonly links: LinkHeader[] = [
-    {
-      id: '6RB_Zf9-rJVGmfoxjkxcH',
-      label: 'Sobre nosotros',
-      href: '',
-      fragment: '',
-    },
-    {
-      id: 'qTxmvaOP3wspCq5DVQGlW',
-      label: 'Proyectos',
-      href: '',
-      fragment: '',
-    },
-    {
-      id: 'Q3crbLQRvjnJ8UceCPT33',
-      label: 'Contacto',
-      href: '',
-      fragment: '',
-    },
-  ];
-}
+export class Header {}
